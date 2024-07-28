aff06300210c5c8e752dfd0ce1a486a63ce76190
fabiofabrice <fabricesumsa2000@gmail.com>
2024-07-28 15:22:52 +0200

    add song bloc provider to main

--- lib/features/music/presentation/pages/playlist_screen.dart
+++ lib/features/music/presentation/pages/playlist_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mental_health/core/theme.dart';
 import 'package:mental_health/features/music/data/datasources/song_remote_datasource.dart';
+import 'package:mental_health/features/music/presentation/bloc/song_bloc.dart';
+import 'package:mental_health/features/music/presentation/bloc/song_state.dart';
 import 'package:mental_health/features/music/presentation/pages/music_player_screen.dart';
 import 'package:http/http.dart' as http;
 
@@ -38,27 +41,41 @@ class PlaylistScreen extends StatelessWidget {
         elevation: 1,
         centerTitle: false,
       ),
-      body: Container(
-        color: DefaultColors.white,
-        child: ListView.builder(
-          itemCount: songs.length,
-          itemBuilder: (context, index){
-            return ListTile(
-              leading: CircleAvatar(
-                backgroundImage: AssetImage(songs[index]['thumbnail']!),
+      body: BlocBuilder<SongBloc, SongState>(
+        builder: (context, state) {
+          if(state is SongLoading){
+            return Center(child: CircularProgressIndicator(),);
+          }
+          else if(state is SongLoaded){
+            return Container(
+              color: DefaultColors.white,
+              child: ListView.builder(
+                  itemCount: state.songs.length,
+                  itemBuilder: (context, index){
+                    return ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage: AssetImage(songs[index]['thumbnail']!),
+                      ),
+                      trailing: Icon(Icons.arrow_forward_ios, size: 15),
+                      title: Text(state.songs[index].title, style: Theme.of(context).textTheme.labelMedium,),
+                      subtitle: Text(state.songs[index].author, style: Theme.of(context).textTheme.labelSmall,),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => MusicPlayerScreen()),
+                        );
+                      },
+                    );
+                  }
               ),
-              trailing: Icon(Icons.arrow_forward_ios, size: 15),
-              title: Text(songs[index]['title']!, style: Theme.of(context).textTheme.labelMedium,),
-              subtitle: Text(songs[index]['artist']!, style: Theme.of(context).textTheme.labelSmall,),
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => MusicPlayerScreen()),
-                );
-              },
             );
           }
-        ),
+          else if(state is SongError){
+            return Center(child: Text(state.message, style: Theme.of(context).textTheme.labelSmall,),);
+          } else {
+            return Center(child: Text('No songs found', style: Theme.of(context).textTheme.labelSmall,),);
+          }
+        },
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mental_health/core/theme.dart';
 import 'package:mental_health/features/meditation/presentation/pages/meditation_screen.dart';
+import 'package:mental_health/features/music/data/datasources/song_remote_datasource.dart';
+import 'package:mental_health/features/music/data/repository/song_repository_impl.dart';
+import 'package:mental_health/features/music/domain/usecases/get_all_songs.dart';
+import 'package:mental_health/features/music/presentation/bloc/song_bloc.dart';
+import 'package:mental_health/features/music/presentation/bloc/song_event.dart';
 import 'package:mental_health/presentation/bottomNavBar/bloc/navigation_bloc.dart';
 import 'package:mental_health/presentation/homePage/home_page.dart';
 import 'package:mental_health/presentation/onboarding/onboarding.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -19,6 +25,17 @@ class MyApp extends StatelessWidget {
         providers: [
           BlocProvider(
             create: (_) => NavigationBloc(),
+          ),
+          BlocProvider(
+              create: (context) => SongBloc(
+                getAllSongs: GetAllSongs(
+                  repository: SongRepositoryImpl(
+                    remoteDataSource: SongRemoteDataSourceImpl(
+                      client: http.Client()
+                    )
+                  )
+                )
+              )..add(FetchSongs())
           )
         ],
         child: MaterialApp(
